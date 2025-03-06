e3f676873e026bd920350c00990320360ad482d0
Jonathan Zuluaga <57453313+Jonathan99-ai@users.noreply.github.com>
2025-03-06 18:09:30 -0500

    Update app_start.sh

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
-#go to the server folder where the file is
-cd /home/ec2-user/server
+#_Change_Working_Directory
+cd /var/www/html
 
-#unzip the folder in the required route for apache2
-unzip TestBuild.zip -d /var/www/html
+# /var/www/html is the folder apache2 http server uses to serve a website
+sudo cp /home/ec2-user/server/dist/* . -r
 
 #restar the apache2 server
 sudo systemctl restart httpd
-
-#_Change_Working_Directory
-#cd /var/www/html
-
-# /var/www/html is the folder apache2 http server uses to serve a website
-#sudo cp /home/ec2-user/TestBuild.zip/* . -r
